69422cc3f47024bb2025ad4bc783561f3b202d1b
JaniceBPC <janice.kierstead@gmail.com>
2019-11-08 07:38:59 -0500

    Move Tasks & Progres tracking from Common.GUI to Common as these objects needed to be referenced within the domain models as well

---
+++ Common/Task Dispatcher/ITask.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Threading.Tasks;
+
+namespace Jbpc.Common
+{
+    public interface ITask
+    {
+        void PerformTask(ITaskBridge taskBridge);
+        string LogMessage { get; }
+        string Description { get; }
+        void Reset();
+        Task Task { get; }
+    }
+}
