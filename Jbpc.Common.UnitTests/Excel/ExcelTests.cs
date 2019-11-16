dd33da99f00fde6e260f0256e67a508ef0b2ce9e
JaniceBPC <janice.kierstead@gmail.com>
2019-11-16 06:46:37 -0500

    .Net Core Migration

---
+++ Jbpc.Common.UnitTests/Excel/ExcelTests.cs
@@ -0,0 +1,24 @@
+﻿using Jbpc.Common.Excel;
+using NUnit.Framework;
+
+namespace Jbpc.Common.UnitTests.Excel
+{
+    [TestFixture]
+    public class ExcelTests
+    {
+        [Test]
+        public void ExpandedTypeName_SimpleType()
+        {
+            var workbook = ExcelOperations.NewWorkbook;
+
+            Assert.NotNull(workbook);
+        }
+        [Test]
+        public void Net_Std()
+        {
+            var workbook = Office.Class1.Application;
+
+            Assert.NotNull(workbook);
+        }
+    }
+}
